3f4166f201644f5ea1d226446c2766a1173e770f
Sus <thrvoid97@gmail.com>
2021-11-27 17:09:04 +0300

    Perfect car upgrade/downgrade

--- Assets/Scripts/CarPartCollector.cs
+++ Assets/Scripts/CarPartCollector.cs
@@ -2,41 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using UnityEngine;
 
 public class CarPartCollector : MonoBehaviour
 {
     [SerializeField] private List<SkinnedMeshRenderer> carMeshes = new List<SkinnedMeshRenderer>();
+
+    public int collectedPartsCount,inBetween,amountForUpgrade, currentMesh, currentBlendShape;
+
+    private TweenerCore<float, float, FloatOptions> currentTween;
     
-    [SerializeField] private int amountForUpgrade;
-    private int firstAmountForUpgrade;
-    
-    [SerializeField] private int upgradeAmountIncrement;
 
-    [Header("0 for instant, 1 for slowest")]
-    [SerializeField] private float blendSmoothSpeed;
-    
-    public int collectedPartsCount;
-
-    public int inBetween,currentBlendShape,currentMesh,awaitingUpgrades,totalAmountNeeded;
-
-    private List<int> totalNeededForEachCar = new List<int>();
-
-
-
-    private void Start()
-    {
-        StartCoroutine(nameof(UpgradeSmooth));
-        firstAmountForUpgrade = amountForUpgrade;
-        
-        for (int i = 0; i < carMeshes.Count; i++)
-        {
-            totalAmountNeeded += (amountForUpgrade + (upgradeAmountIncrement * i)) *
-                                 carMeshes[i].sharedMesh.blendShapeCount;
-            totalNeededForEachCar.Add((amountForUpgrade + (upgradeAmountIncrement * i)) * carMeshes[i].sharedMesh.blendShapeCount);
-        }
-    }
-    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(gameObject.tag))
@@ -49,28 +27,38 @@ public class CarPartCollector : MonoBehaviour
 
     private void CheckCarUpgrade()
     {
-        if (collectedPartsCount <= totalAmountNeeded)
+        inBetween++;
+        
+        if (inBetween == amountForUpgrade)
         {
-            inBetween++;
+            inBetween = 0;
+
+            currentTween?.Complete();
             
-            if (inBetween == amountForUpgrade)
+            var smoothValue = 100f;
+
+            if (currentBlendShape == carMeshes[currentMesh].sharedMesh.blendShapeCount)
             {
-                inBetween = 0;
-                
-                awaitingUpgrades++;
-                
-                if (collectedPartsCount == totalNeededForEachCar[currentMesh])
-                {
-                    amountForUpgrade += upgradeAmountIncrement;
-                }
+                UpgradeCar();
+                return;
             }
+
+            currentTween = DOTween.To(() => smoothValue, x => smoothValue = x, 0f, 2f)
+                .OnUpdate(() =>
+                {
+                    carMeshes[currentMesh].SetBlendShapeWeight(currentBlendShape, smoothValue);
+                }).OnComplete(() =>
+                {
+                    currentBlendShape = Mathf.Clamp(currentBlendShape += 1, 0,
+                        carMeshes[currentMesh].sharedMesh.blendShapeCount);
+                });
         }
     }
 
-    public void UpgradeCar()
+    private void UpgradeCar()
     {
-        if(currentMesh + 1 >= carMeshes.Count) { return; }
-        ResetValues();
+        if(currentMesh == carMeshes.Count - 1) { return; }
+        currentBlendShape = 0;
 
         carMeshes[currentMesh].gameObject.SetActive(false);
         currentMesh++;
@@ -79,65 +67,34 @@ public class CarPartCollector : MonoBehaviour
 
     public void DowngradeCar()
     {
-        var blocksToPopCount = Mathf.Clamp(inBetween + (amountForUpgrade * (currentBlendShape +1)),0,collectedPartsCount);
-
-        collectedPartsCount -= blocksToPopCount;
-
-        awaitingUpgrades = 0;
+        currentTween?.Complete();
         
-        ResetValues();
+        var blocksToRemove = Mathf.Clamp(inBetween + amountForUpgrade * currentBlendShape + (carMeshes[Mathf.Clamp(currentMesh - 1,0,carMeshes.Count)].sharedMesh.blendShapeCount + 1) * amountForUpgrade, 0,collectedPartsCount);
         
-        amountForUpgrade = Mathf.Clamp(amountForUpgrade - upgradeAmountIncrement,firstAmountForUpgrade,99999);
-
-        if (currentMesh - 1 <= carMeshes.Count) { return; }
+        collectedPartsCount -= blocksToRemove;
         
+        inBetween = 0;
+        
+        currentBlendShape = 0;
+        
+        ResetShapes();
+        
+        if (currentMesh == 0) { return; }
 
         carMeshes[currentMesh].gameObject.SetActive(false);
         currentMesh--;
         carMeshes[currentMesh].gameObject.SetActive(true);
+        
+        ResetShapes();
     }
 
-    private void ResetValues()
+    private void ResetShapes()
     {
         for(int i=0; i< carMeshes[currentMesh].sharedMesh.blendShapeCount; i++)
         {
             carMeshes[currentMesh].SetBlendShapeWeight(i, 100f);
         }
-        currentBlendShape = 0;
-        inBetween = 0;
     }
-
-    private IEnumerator UpgradeSmooth()
-    {
-        var value = 100f;
-        while (true)
-        {
-            if (awaitingUpgrades > 0)
-            {
-                if (value > 0f)
-                {
-                    var clampValue = Mathf.Clamp(value -= 1f - blendSmoothSpeed, 0f, 100f);
-                    carMeshes[currentMesh].SetBlendShapeWeight(currentBlendShape, clampValue);
-                    yield return null;
-                }
-                else
-                {
-                    currentBlendShape = Mathf.Clamp(currentBlendShape+=1, 0, carMeshes[currentMesh].sharedMesh.blendShapeCount);
-                    value = 100f; 
-                    awaitingUpgrades--;
-                    
-                    if (currentBlendShape == carMeshes[0].sharedMesh.blendShapeCount)
-                    {
-                        UpgradeCar();
-                    }
-                    
-                    yield return null;
-                }
-                
-                
-            }
-            
-            yield return null;
-        }
-    }
+    
+    
 }
